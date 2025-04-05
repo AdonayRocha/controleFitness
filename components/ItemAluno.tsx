a55862a1184ad8cb5ea9c5826444e6214f14df6f
adonayrocha <adonay.rodrigues.rocha@gmail.com>
2025-04-05 04:23:18 -0300

    NewUpdates

--- components/ItemAluno.tsx
+++ components/ItemAluno.tsx
@@ -2,35 +2,26 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Aluno } from '../App';
 
-interface Props {
-    aluno: Aluno;
-}
-
-export default function AlunoItem({ aluno }: Props) {
+export default function ItemAluno({ nome, instrutor, plano }: Aluno) {
     return (
-        <View style={styles.container}>
-            <Text style={styles.nome}>{aluno.nome}</Text>
-            <Text style={styles.texto}>{aluno.instrutor}</Text>
-            <Text style={styles.texto}>{aluno.plano}</Text>
+        <View style={styles.card}>
+            <Text style={styles.nomeTitulo}>{nome}</Text>
+            <Text>{instrutor}</Text>
+            <Text>{plano}</Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'yellow',
+    card: {
         borderColor: 'red',
         borderWidth: 2,
         borderRadius: 16,
         padding: 10,
         marginBottom: 10,
+        backgroundColor: 'yellow',
     },
-    nome: {
-        fontWeight: 'bold',
-        fontSize: 18,
-        marginBottom: 4,
-    },
-    texto: {
-        fontSize: 14,
-    },
+    nomeTitulo: {
+        fontSize: 24, 
+    }
 });
